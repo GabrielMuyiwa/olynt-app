ad731a7a12cd0a9d45d02a6930d73aaff161ba39
gabriel muyiwa <gabrielroyalpriest@gmail.com>
2026-05-18 12:35:34 +0100

    Added monetag verification

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+
+        {/* MONETAG VERIFICATION CODE */}
+
+        <meta name="monetag" content="b78d775e910c928e5216baa1b10d6235"></meta>
+
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
